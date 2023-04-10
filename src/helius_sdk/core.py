c70cdfacce778f3af0fee83331d10a4a4e6adc2d
Mayur Chougule <mayurchougule1@gmail.com>
2023-04-09 23:43:20 -0400

    NFT fingerprint and transactions API added

--- src/helius_sdk/core.py
+++ src/helius_sdk/core.py
@@ -7,17 +7,15 @@ import polars as pl
 
 
 def load_config(config_path="conf/base.yml"):
-    try:
-        if os.path.exists(config_path):
-            with open(config_path, "r") as config_file:
-                config = yaml.safe_load(config_file)
-            return config["helius"]
-        else:
-            return {
-                "base_url": os.getenv("BASE_URL") or "https://api.helius.xyz/v1",
-                "api_key": os.getenv("API_KEY"),
-            }
-    except Exception as e:
+    if os.path.exists(config_path):
+        with open(config_path, "r") as config_file:
+            config = yaml.safe_load(config_file)
+        return config["helius"]
+    else:
+        return {
+            "base_url": os.getenv("BASE_URL") or "https://api.helius.xyz/v1",
+            "api_key": os.getenv("API_KEY"),
+        }
 
 
 CONFIG = load_config()
